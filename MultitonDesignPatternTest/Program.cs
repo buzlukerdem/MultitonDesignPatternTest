54f1164a9e6866db21c08607c658e26c35091675
buzlukerdem <buzlukerdem@gmail.com>
2025-01-17 18:50:26 +0300

    Add project files.

---
+++ MultitonDesignPatternTest/Program.cs
@@ -0,0 +1,30 @@
+﻿var db1 = Database.GetDatabaseInstance("MsSQL");
+db1.ConnectionString("/connection...");
+var db4 = Database.GetDatabaseInstance("MsSQL");
+
+var db2 = Database.GetDatabaseInstance("Oracle");
+var db3 = Database.GetDatabaseInstance("MySQL");
+
+class Database
+{
+    static string _key = "";
+    private Database()
+    {
+        Console.WriteLine($"{_key} nesnesi üretildi...");
+    }
+    static Dictionary<string, Database> _databases = new();
+
+    public static Database GetDatabaseInstance(string key)
+    {
+        _key = key;
+        // Key e karşılık instance kontrolü.
+        if (!_databases.ContainsKey(key))
+            _databases[key] = new Database();
+        return _databases[key];
+    }
+
+    public void ConnectionString(string connectionString)
+    {
+    }
+
+}
